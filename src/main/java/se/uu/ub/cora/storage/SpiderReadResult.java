fbece13407af4175e462e17dd96f74b0e336ed1c
madde <madde@dd647c6c5f9a>
2019-06-18 08:57:53 +0000

    coraData instead of bookkeeper data

--- src/main/java/se/uu/ub/cora/storage/SpiderReadResult.java
+++ src/main/java/se/uu/ub/cora/storage/SpiderReadResult.java
@@ -20,7 +20,7 @@ package se.uu.ub.cora.storage;
 
 import java.util.List;
 
-import se.uu.ub.cora.bookkeeper.data.DataGroup;
+import se.uu.ub.cora.data.DataGroup;
 
 public final class SpiderReadResult {
 
