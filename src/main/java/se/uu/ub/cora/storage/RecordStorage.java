dd99f5c4ce8e80e2348dd27ad845805b648a195e
perebartrolisimo <pere.bartroli@ub.uu.se>
2021-05-27 13:48:39 +0000

    to be continued...

--- src/main/java/se/uu/ub/cora/storage/RecordStorage.java
+++ src/main/java/se/uu/ub/cora/storage/RecordStorage.java
@@ -20,6 +20,7 @@
 package se.uu.ub.cora.storage;
 
 import java.util.Collection;
+import java.util.List;
 
 import se.uu.ub.cora.data.DataGroup;
 
@@ -28,6 +29,20 @@ import se.uu.ub.cora.data.DataGroup;
  * system. This interface makes the storage implementation decoupled from the rest of the system
  * enabling different storage solutions to be developed and used depending on the needs of the
  * current system.
+ * <p>
+ * The end goal is that implementing storage solutions should not have to know about implementations
+ * details for recordTypes, and should be provided the information needed to handle abstract types,
+ * without having to read and understand the set of existing record types.<br>
+ * This is a work in progress as can be seen in
+ * {@link #getTotalNumberOfAbstractRecords(String, List, DataGroup)}.
+ * <p>
+ * list of thins probably needed:<br>
+ * <ul>
+ * <li>read needs a new method readAbstractList(String abstractType, List<String> implementingTypes,
+ * DataGroup filter)</li>
+ * <li>readAbstractList needs a new parameter List<String> implementingTypes</li>
+ * </ul>
+ * 
  */
 public interface RecordStorage {
 
@@ -69,17 +84,15 @@ public interface RecordStorage {
 
 	/**
 	 * getTotalNumberOfRecords should return the number of records that are stored under the
-	 * specified type. <br>
-	 * If the record type is abstract, the returned number should be all stored records for all
-	 * implementing record types that are children to the specified abstract type.
+	 * specified type.
 	 * <p>
-	 * If a filter is specified the total number of records should reflect only those wich match the
-	 * filter. Filter information is based on the collectedTerms / storageTerms entered together
+	 * If a filter is specified the total number of records should reflect only those which match
+	 * the filter. Filter information is based on the collectedTerms / storageTerms entered together
 	 * with the record when creating or updating the record.<br>
 	 * If the filter specifies a specific range of records to return, the range should be ignored
 	 * and the returned total number of records should be the total number of records stored for the
 	 * type.<br>
-	 * If the filter contains no include or exclude information all records should ,mnbe counted.
+	 * If the filter contains no include or exclude information should all records be counted.
 	 * 
 	 * @param type
 	 *            A String with the record type
@@ -91,7 +104,29 @@ public interface RecordStorage {
 	 */
 	long getTotalNumberOfRecords(String type, DataGroup filter);
 
-	// needs more thoughts... :(
-	// long getTotalNumberOfAbstractRecords(String type, List<String> implementingTypes,
-	// DataGroup filter);
+	/**
+	 * getTotalNumberOfAbstractRecords should return the number of records that belong to the
+	 * specified abstract type. The returned number should be all stored records for the specified
+	 * list of implementing record types.
+	 * <p>
+	 * If a filter is specified the total number of records should reflect only those which match
+	 * the filter. Filter information is based on the collectedTerms / storageTerms entered together
+	 * with the record when creating or updating the record.<br>
+	 * If the filter specifies a specific range of records to return, the range should be ignored
+	 * and the returned total number of records should be the total number of records stored for the
+	 * abstract type.<br>
+	 * If the filter contains no include or exclude information should all records be counted.
+	 * 
+	 * @param abstractType
+	 *            A String with the abstract record type
+	 * @param implementingTypes
+	 *            A List with the implementing record types for the specified abstract type
+	 * @param filter
+	 *            A {@link DataGroup} with filter information about which subset of records to
+	 *            count.
+	 * @return a long with the number of records that exist in storage for the specified list of
+	 *         implementing types and that matches the specified filter
+	 */
+	long getTotalNumberOfAbstractRecords(String abstractType, List<String> implementingTypes,
+			DataGroup filter);
 }
