6d570b43ae7828cff162a5ccecda017f53e60f67
pere <pere@c77ce1f34106>
2023-08-28 15:01:56 +0000

    CORA-2236 (CORA-2229) Add new exception

---
+++ src/test/java/se/uu/ub/cora/storage/ResourceConflictExceptionTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2015 2021 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package se.uu.ub.cora.storage;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class ResourceConflictExceptionTest {
+	@Test
+	public void testInit() {
+		ResourceConflictException conflict = ResourceConflictException.withMessage("message");
+
+		Assert.assertEquals(conflict.getMessage(), "message");
+	}
+
+	@Test
+	public void testInitWithException() {
+		Exception exception = new Exception();
+		ResourceConflictException conflict = ResourceConflictException
+				.withMessageAndException("message", exception);
+
+		assertEquals(conflict.getMessage(), "message");
+		assertEquals(conflict.getCause(), exception);
+	}
+}
